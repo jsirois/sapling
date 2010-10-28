4e89a3aced1ee238fee9a5d0ca38e8200df0f9b3
jsirois <jsirois@twitter.com>
2010-10-28 13:45:23 -0600

    A bit of doc for lib.with_line_numbers

--- saplib/lib.py
+++ saplib/lib.py
@@ -1,7 +1,7 @@
 import os
 
 def find(iterable, predicate, default = None):
-  """ Finds and returns the first item in iterable that passes the supplied predicate.  If not item
+  """Finds and returns the first item in iterable that passes the supplied predicate.  If not item
   matches and a default was not specified then a KeyError is raised; otherwise, the default is
   returned"""
 
@@ -18,6 +18,15 @@ def find(iterable, predicate, default = None):
     return default
 
 def with_line_numbers(string):
+  """Adds line numbers to the given string in a right-justified left hand column.  For example:
+A two line
+string
+
+would be transformed into:
+1 A two line
+2 string
+  """
+
   lines = string.splitlines()
   if len(lines) == 0:
     lines.append(string)
