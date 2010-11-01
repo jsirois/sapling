2868aedc2d047652450938616b18d5ad0655c1ac
jsirois <jsirois@twitter.com>
2010-11-01 08:00:18 -0600

    Fixup setup.py requires -> install_requires, confirmed this works to pull transitive dependencies with both easy_install and pip

--- setup.py
+++ setup.py
@@ -2,35 +2,36 @@ from distutils.core import setup
 
 setup(
     name = 'sapling',
-    version = '0.0.1',
+    version = '0.0.3',
 
-    author = "John Sirois",
-    author_email = "john.sirois@gmail.com",
-    description = "A git porcelain to manage bidirectional subtree syncing with foreign git "
-                  "repositories",
-    license = "Apache License Version 2.0",
-    url = "http://github.com/jsirois/sapling",
+    author = 'John Sirois',
+    author_email = 'john.sirois@gmail.com',
+    description = 'A git porcelain to manage bidirectional subtree syncing with foreign git '
+                  'repositories',
+    license = 'Apache License Version 2.0',
+    url = 'http://github.com/jsirois/sapling',
 
-    provides = "sapling",
-    requires = [
-      "GitPython (>= 0.3)"
-    ],
+    provides = 'sapling',
+    install_requires = (
+      'gitdb >= 0.5.1',
+      'GitPython > 0.2, < 0.4',
+    ),
 
     packages = [ 'saplib' ],
     scripts = [ 'sapling.py' ],
 
     classifiers = [
-      "Programming Language :: Python",
-      "Programming Language :: Python :: 2.6",
-      "Development Status :: 2 - Pre-Alpha",
-      "Environment :: Console",
-      "Intended Audience :: Developers",
-      "License :: OSI Approved :: Apache Software License",
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.6',
+      'Development Status :: 2 - Pre-Alpha',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: Apache Software License',
 
       # TODO(jsirois): the sapling.py --install action is actually unix/symlink dependant -
       # perhaps detect windows and just copy the sapling.py script to git-core/git-sap ?
-      "Operating System :: OS Independent",
+      'Operating System :: OS Independent',
 
-      "Topic :: Software Development :: Version Control"
+      'Topic :: Software Development :: Version Control'
     ],
 )
